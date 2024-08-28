c6ddcea9e509a6a62bdfd2269f70569dd3a6197d
pv-nihar <165635623+pv-nihar@users.noreply.github.com>
2024-08-28 11:51:59 +0530

    added debugger

--- middleware.js
+++ middleware.js
@@ -4,16 +4,17 @@ export function middleware(request) {
   const { pathname } = request.nextUrl;
   const authToken = request.cookies.get("authToken")?.value;
 
-  // Define routes
+  console.log("Auth Token:", authToken);
+  console.log("Requested Path:", pathname);
+
   const isAuthRoute = ["/login"].includes(pathname);
   const isProtectedRoute = ["/admin/create", "/admin"].some((route) =>
     pathname.startsWith(route)
   );
-  // Redirect authenticated users away from auth pages
+
   if (authToken && isAuthRoute) {
     return NextResponse.redirect(new URL("/admin", request.url));
   }
-  // Redirect unauthenticated users to login page or return 401 for API routes
   if (!authToken && isProtectedRoute) {
     if (pathname.startsWith("/api")) {
       return NextResponse.json(
@@ -24,18 +25,14 @@ export function middleware(request) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
 
-  // Allow the request to proceed
   return NextResponse.next();
 }
 
-// Configure paths where middleware should run
 export const config = {
   matcher: [
     "/admin",
     "/login",
     "/signup",
-    "/add-task",
-    "/show-tasks",
     "/profile/:path*",
     "/api/:path*",
   ],
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,32 +1,38 @@
-import { Jost } from 'next/font/google'
-import './globals.css'
-import Header from '@/components/navigationbar/Header'
-import Footer from '@/components/footer/Footer'
-import { Toaster } from 'sonner'
+import { Jost } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/navigationbar/Header";
+import Footer from "@/components/footer/Footer";
+import { Toaster } from "sonner";
 
-const jost = Jost({ subsets: ['latin'] })
+const jost = Jost({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Mcad | Premier Architecture Firm in Australia',
-  description: 'Mcad is a leading architecture firm in Australia, specializing in innovative and sustainable design solutions for residential, commercial, and public spaces.',
-  keywords: 'Mcad, architecture, Australia, sustainable design, residential architecture, commercial architecture, public spaces, architectural services',
-  author: 'Mcad Architecture Firm',
-  viewport: 'width=device-width, initial-scale=1',
-  robots: 'index, follow',
-  charset: 'UTF-8',
+  title: "Mcad | Premier Architecture Firm in Australia",
+  description:
+    "Mcad is a leading architecture firm in Australia, specializing in innovative and sustainable design solutions for residential, commercial, and public spaces.",
+  keywords:
+    "Mcad, architecture, Australia, sustainable design, residential architecture, commercial architecture, public spaces, architectural services",
+  author: "Mcad Architecture Firm",
+  robots: "index, follow",
+  charset: "UTF-8",
 };
-
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      
-      <body className={`${jost.className } max-w-[2100px] mx-auto` } >
-      <Header />
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        />
+      </head>
+      <body className={`${jost.className} max-w-[2100px] mx-auto`}>
+        <Header />
         {children}
-        <Toaster richColors  />
+        <Toaster richColors />
 
-        <Footer /></body>
+        <Footer />
+      </body>
     </html>
-  )
+  );
 }
